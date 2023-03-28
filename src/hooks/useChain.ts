74131bd50df7af553d9dab49bf58f5157fb28b34
Ardy <ardy@defi.sucks>
2023-03-28 10:18:39 -0400

    chore: add wagmi and rainbowkit

--- src/hooks/useChain.ts
+++
@@ -1,9 +0,0 @@
-import { useMemo } from 'react';
-import { Chains } from '../utils/chains';
-import { useWeb3Context } from '../components/libs/web3-data-provider/Web3Provider';
-
-export const useChain = () => {
-  const { chainId } = useWeb3Context();
-
-  return useMemo(() => Chains.getInfo(chainId), [chainId]);
-};
