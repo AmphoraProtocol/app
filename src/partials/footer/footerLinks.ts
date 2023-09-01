7eca27eb0a9c7c203e593414a5e0c87f7358fa56
OpenOrg-gg <adam@openorg-gg>
2023-09-01 19:19:37 -0400

    fix: socials

--- src/partials/footer/footerLinks.ts
+++ src/partials/footer/footerLinks.ts
@@ -3,7 +3,7 @@ export default [
     title: 'Protocol',
     links: [
       // { label: 'Whitepaper', href: '#/whitepaper' },
-      { label: 'Docs', href: 'https://app.gitbook.com/s/f8xPtDNnJGPzpmL2yjns/' },
+      { label: 'Docs', href: 'https://amphora-protocol.gitbook.io/amphora-protocol/' },
       // { label: 'Analytics', href: '#' },
       // { label: 'Terms of Use', href: '#/terms' },
     ],
@@ -19,7 +19,7 @@ export default [
     title: 'Community',
     links: [
       { label: 'Twitter', href: '#' },
-      { label: 'Discord', href: '#' },
+      { label: 'Discord', href: 'https://discord.gg/EXSYFTgwp6' },
     ],
   },
   {
